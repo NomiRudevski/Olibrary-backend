c1af364da6cc1e200a13207e931672a402fd5ca4
NomiRudevski <nomi.rudevski@gmail.com>
2024-07-08 21:49:49 +0300

    gl

--- backend/app.py
+++ backend/app.py
@@ -9,6 +9,8 @@ from .models import *
 app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+app.config['SESSION_COOKIE_SECURE'] = True
+app.config['SESSION_COOKIE_SAMESITE'] = 'None'
 app.secret_key = 'njf74^&*^ndk'
 
 # Configure SQLAlchemy engine
